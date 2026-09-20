08810b94e2b587db08ee849c136d2f3c3a40e35a
nexohubyt-oss <nexohubyt@gmail.com>
2026-09-20 08:54:40 +0600

    Delete passport-room directory

--- passport-room/src/client/membership-boot.js
+++
@@ -1,8 +0,0 @@
-// Starts the membership engine once the page is ready.
-import { initMembership } from "./membership.js";
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", () => initMembership());
-} else {
-  initMembership();
-}
